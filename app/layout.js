74df3cf4f5a4e1036ef501a4d860b47c3a6ced0d
Mark Brest <MarkAlanBrest@gmail.com>
2026-02-19 18:12:37 -0500

    p

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,5 @@
 "use client";
 
-export const metadata = {
-  title: "Resume Builder",
-  description: "NCST Resume Builder",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
