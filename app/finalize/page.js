6b4504da22c3520e7ee67d63d6e8e31a6038dbac
Mark Brest <MarkAlanBrest@gmail.com>
2026-02-19 18:52:10 -0500

    p

--- app/finalize/page.js
+++ app/finalize/page.js
@@ -2,7 +2,7 @@
 
 export default function FinalizePage() {
   return (
-    <div style={{ padding: "20px" }}>
+    <div>
       <h1>Finalize Resume</h1>
       <p>Your resume is almost ready.</p>
     </div>
